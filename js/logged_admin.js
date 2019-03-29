162e7de643232f5199974c0e434ab377b9508409
Kartikay Goyle <kartikaygoyle@Kartikays-MacBook-Pro.local>
2019-03-29 10:30:10 -0400

    Removed original file structure

--- js/logged_admin.js
+++
@@ -1,9 +0,0 @@
-const new_var = document.querySelectorAll('.deleteUser');
-console.log(new_var);
-var i;
-for (i=0; i<new_var.length; i++)
-{ new_var[i].addEventListener('click',delUser); }
-
-function delUser(e) {
-	e.target.parentElement.parentElement.remove();
-}
