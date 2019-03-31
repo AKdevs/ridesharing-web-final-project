ae3eb8116494e3820bf8ead360078d77ec89cc11
Kartikay Goyle <kartikaygoyle@Kartikays-MacBook-Pro.local>
2019-03-31 13:22:18 -0400

    Logout and login functionality works now

--- routes/users.js
+++ routes/users.js
@@ -21,17 +21,52 @@ router.get('/login', function(req, res, next) {
   res.render('login', {title:'Login'});
 });
 
-router.get('/logged', function(req, res, next) {
-  console.log("Person is now logged")
-  var user = req.session.username;
-  //console.log(req.body.firstname);
-    User.find({username: req.user.username}, function(err, docs){
-        console.log(docs)
-        if (err) {console.log("user not found"); res.status(404).send();}
-        else{
-            res.render('logged',{user: {"firstname": docs[0].firstname.toString(), "lastname": docs[0].lastname.toString(), "username": docs[0].username.toString(), "email": docs[0].email.toString(),"phone": docs[0].phone.toString() }});
-        }
+
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+        console.log("DESTROTYYYYYYYYYYY")
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
     });
+  }
+});
+
+
+function requiresLogin(req, res, next) {
+  if (!req.user) {
+    //console.log(req.user.username)
+    var err = new Error('You must be logged in to view this page.');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+
+
+router.get('/logged',  function(req, res, next) {
+    
+    console.log(req.session);
+    console.log(req.user);
+    
+    if(!req.user || !req.session){
+        var err = new Error('You must be logged in to view this page.');
+        err.status = 401;
+        return next(err);
+    }
+      console.log("Person is now logged")
+      //console.log(req.body.firstname);
+        User.find({username: req.user.username}, function(err, docs){
+            console.log(docs)
+            if (err) {console.log("user not found"); res.status(404).send();}
+            else{
+                res.render('logged',{user: {"firstname": docs[0].firstname.toString(), "lastname": docs[0].lastname.toString(), "username": docs[0].username.toString(), "email": docs[0].email.toString(),"phone": docs[0].phone.toString() }});
+            }
+        });
 });
 
 router.post('/logged', function(req, res, next) {
