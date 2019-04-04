8f25bf074aceb991d53010db3cf49f3f8029661e
Kartikay Goyle <kartikaygoyle@Kartikays-MacBook-Pro.local>
2019-04-04 02:04:53 -0400

    Added getusername function

--- routes/users.js
+++ routes/users.js
@@ -58,6 +58,7 @@ router.get('/logged',  function(req, res, next) {
         return next(err);
     }
       console.log("Person is now logged")
+      console.log(req.user.username);
       //console.log(req.body.firstname);
         User.find({username: req.user.username}, function(err, docs){
             console.log(docs)
@@ -66,26 +67,27 @@ router.get('/logged',  function(req, res, next) {
                 res.render('logged',{user: {"firstname": docs[0].firstname.toString(), "lastname": docs[0].lastname.toString(), "username": docs[0].username.toString(), "email": docs[0].email.toString(),"phone": docs[0].phone.toString() }});
             }
         });
+        res.location('/users/getloggedusername');
+        res.redirect('/users/getloggedusername');
 });
 
 router.get('/getloggedusername', function(req, res, next){
     
-    
-    if (!req.user) {
-    //console.log(req.user.username)
+    if(!req.user || !req.session){
         var err = new Error('You must be logged in to view this page.');
         err.status = 401;
         return next(err);
     }
-    else {
-        console.log(req.user.username);
-        res.send(req.user.username);
-        
-    }
-    
-    
-//    console.log(req.user.username);
-//    res.send(req.user.username);
+      console.log("Username of the person is")
+      console.log(req.user.username);
+      //console.log(req.body.firstname);
+        User.find({username: req.user.username}, function(err, docs){
+            console.log(docs)
+            if (err) {console.log("user not found"); res.status(404).send();}
+            else{
+                res.send(docs[0].username.toString());
+            }
+        });
 })
 // router.get('/users/:username', function(req, res, next) {
 //     const username = req.params.username;
@@ -101,7 +103,7 @@ router.get('/getloggedusername', function(req, res, next){
 
 router.post('/logged', function(req, res, next) {
     console.log("USER LOGGED POST")
-    //console.log(req.user);
+    console.log(req.user.username);
 
 
     // User.findOneAndUpdate({
